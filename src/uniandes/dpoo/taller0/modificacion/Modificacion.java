3aa42698e931c077689c3bdf8ce7e2ff2bca6ee9
Eliana Palacio <e.palacio@uniandes.edu.co>
2022-01-31 21:43:33 -0500

    Arreglo Definitivo

--- src/uniandes/dpoo/taller0/modificacion/Modificacion.java
+++ src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -2,6 +2,7 @@ package uniandes.dpoo.taller0.modificacion;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Map;
 
 import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
 import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
@@ -13,13 +14,18 @@ public class Modificacion {
 		
 		try {
 			CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
-			System.out.println(calc.paisConMasMedallistas());
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Calculando el país con más medallas...");
+			Map<String, Integer> paisConMasMedallas = calc.paisConMasMedallistas();
+			System.out.println("El país con más medallas fue: " + paisConMasMedallas);
+		} catch (FileNotFoundException e) 
+		{
+			System.out.println("ERROR: el archivo indicado no se encontró.");
+		}
+
+		catch (IOException e)
+		{
+			System.out.println("ERROR: hubo un problema leyendo el archivo.");
+			System.out.println(e.getMessage());
 		}
 	}
 }
